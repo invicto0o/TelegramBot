c0f1aed1a47af28b5b922efcd4bfeeb5a59bfecf
INVICTO <0y1z2n3@gmail.com>
2026-09-19 16:14:56 +0300

    Update bot token and add polling functionality

--- phutos_bot.py
+++ phutos_bot.py
@@ -3,7 +3,7 @@ import telebot
 import yt_dlp
 
 # حط التوكن حقك هنا اللي طلعته من BotFather
-TOKEN = "8932107945:AAHKx7CuIRCXNq0D-NB-trA2op4Aqa6YrQ0"
+TOKEN = "8932107945:AAGfl0QWHOHv27bGj_zTqr7Z9I7AoAATW34"
 bot = telebot.TeleBot(TOKEN)
 
 
